a68953c0cbf90e53fc7738f81bfd0a13a02135ab
HyperVoid01 <166482458+HyperVoid01@users.noreply.github.com>
2024-04-10 10:39:57 -0100

    Added UI for proejct.

--- GitHubPractice/Form1.Designer.cs
+++ GitHubPractice/Form1.Designer.cs
@@ -28,13 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtPlayerName = new System.Windows.Forms.TextBox();
+            this.lblPlayerName = new System.Windows.Forms.Label();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.lblDragonName = new System.Windows.Forms.Label();
+            this.txtDragonName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPlayerName
+            // 
+            this.txtPlayerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlayerName.Location = new System.Drawing.Point(12, 64);
+            this.txtPlayerName.Multiline = true;
+            this.txtPlayerName.Name = "txtPlayerName";
+            this.txtPlayerName.Size = new System.Drawing.Size(776, 51);
+            this.txtPlayerName.TabIndex = 0;
+            // 
+            // lblPlayerName
+            // 
+            this.lblPlayerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerName.Location = new System.Drawing.Point(12, 38);
+            this.lblPlayerName.Name = "lblPlayerName";
+            this.lblPlayerName.Size = new System.Drawing.Size(776, 23);
+            this.lblPlayerName.TabIndex = 1;
+            this.lblPlayerName.Text = "Player Name:";
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbLog.Location = new System.Drawing.Point(12, 314);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.Size = new System.Drawing.Size(776, 109);
+            this.rtbLog.TabIndex = 2;
+            this.rtbLog.Text = "";
+            // 
+            // lblDragonName
+            // 
+            this.lblDragonName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDragonName.Location = new System.Drawing.Point(15, 137);
+            this.lblDragonName.Name = "lblDragonName";
+            this.lblDragonName.Size = new System.Drawing.Size(776, 23);
+            this.lblDragonName.TabIndex = 4;
+            this.lblDragonName.Text = "Dragon Name:";
+            // 
+            // txtDragonName
+            // 
+            this.txtDragonName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDragonName.Location = new System.Drawing.Point(15, 163);
+            this.txtDragonName.Multiline = true;
+            this.txtDragonName.Name = "txtDragonName";
+            this.txtDragonName.Size = new System.Drawing.Size(776, 49);
+            this.txtDragonName.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Gray;
+            this.btnSave.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(15, 225);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(776, 73);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblDragonName);
+            this.Controls.Add(this.txtDragonName);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.lblPlayerName);
+            this.Controls.Add(this.txtPlayerName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtPlayerName;
+        private System.Windows.Forms.Label lblPlayerName;
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.Label lblDragonName;
+        private System.Windows.Forms.TextBox txtDragonName;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
